f381a01af05def980e4b0eab2c6ac727befcd94e
Ailln <kinggreenhall@gmail.com>
2024-01-08 23:26:39 +0800

    fix run.sh

--- script/run.sh
+++ script/run.sh
@@ -5,7 +5,7 @@ echo "${PROJECT_NAME} deploying..."
 
 docker run -d --restart=always \
   --name $PROJECT_NAME \
-  -p 9999:9999 \
+  -p $PORT:9999 \
   -v ./conf/:/app/conf \
   -e ENV=prod \
   $PROJECT_NAME
